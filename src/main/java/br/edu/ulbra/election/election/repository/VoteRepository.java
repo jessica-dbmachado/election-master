0ab40de2d7147ae0316c0d3fb084792cb5a2ab92
jessica-dbmachado <44639567+jessica-dbmachado@users.noreply.github.com>
2018-11-21 11:13:02 -0200

    v3

--- src/main/java/br/edu/ulbra/election/election/repository/VoteRepository.java
+++ src/main/java/br/edu/ulbra/election/election/repository/VoteRepository.java
@@ -1,9 +1,13 @@
 package br.edu.ulbra.election.election.repository;
-
+import java.util.List;
 import br.edu.ulbra.election.election.model.Vote;
 import org.springframework.data.repository.CrudRepository;
 
 public interface VoteRepository extends CrudRepository<Vote, Long> {
 	
 	Vote findByElectionId(Long electionId);
+	
+	List <Vote> findByCandidateId(Long candidateId);
+	
+	List<Vote> getByVoterId(Long voterId);
 }
